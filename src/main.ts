16f095096bd14f3817292ad567ffe931dfb110a8
Vadim <anovic123@divine.band>
2024-10-16 20:25:55 +0300

    init

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
+import { AppModule } from './app.module';
+
+import { applyAppSettings } from './settings/apply-app-settings';
+import { ConfigurationType } from './settings/configuration';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors()
+
+  applyAppSettings(app)
+
+  const configService = app.get(ConfigService<ConfigurationType, true>)
+
+  const apiSettings = configService.get('apiSettings', { infer: true })
+  const environmentSettings = configService.get('environmentSettings', {
+    infer: true
+  })
+
+  const port = apiSettings.PORT
+
+  await app.listen(port, () => {
+    console.log('PORT: ', port)
+    console.log('ENV:', environmentSettings.currentEnv)
+  });
+}
+bootstrap();
