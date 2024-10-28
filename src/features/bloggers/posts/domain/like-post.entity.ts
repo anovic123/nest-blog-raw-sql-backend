9ab1290c35f0ce496bbba3c1a530ce53de9a991c
anovic123 <anovicjosip@gmail.com>
2024-10-28 21:22:18 +0200

    features: like post entity

---
+++ src/features/bloggers/posts/domain/like-post.entity.ts
@@ -0,0 +1,35 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { LikePostStatus } from "../../blogs/api/models/output";
+
+@Entity('like-posts')
+export class LikePosts {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: false
+  })
+  authorId: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: false
+  })
+  status: LikePostStatus
+
+  @Column({
+    type: 'varchar',
+    nullable: false
+  })
+  postId: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: false
+  })
+  login: string
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
