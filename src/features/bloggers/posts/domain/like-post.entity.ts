36fbfadb5d7ebc165fae3cdff67216867d393387
anovic123 <anovicjosip@gmail.com>
2024-12-22 18:50:27 +0200

    features: migrate post likes to typeorm

--- src/features/bloggers/posts/domain/like-post.entity.ts
+++ src/features/bloggers/posts/domain/like-post.entity.ts
@@ -1,27 +1,19 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
-import { LikePostStatus } from "../../blogs/api/models/output";
+import { Entity, PrimaryColumn, Column, CreateDateColumn, Unique } from 'typeorm';
+import { LikePostStatus } from '../../blogs/api/models/output';
 
 @Entity('like-posts')
+@Unique(['authorId', 'postId'])
 export class LikePosts {
   @PrimaryColumn('uuid')
   id: string;
 
-  @Column({
-    type: 'varchar',
-    nullable: false
-  })
+  @Column({ type: 'varchar', nullable: false })
   authorId: string;
 
-  @Column({
-    type: 'varchar',
-    nullable: false
-  })
-  status: LikePostStatus
+  @Column({ type: 'varchar', nullable: false })
+  status: LikePostStatus;
 
-  @Column({
-    type: 'varchar',
-    nullable: false
-  })
+  @Column({ type: 'varchar', nullable: false })
   postId: string;
 
   @CreateDateColumn()
